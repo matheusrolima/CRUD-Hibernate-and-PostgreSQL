a7e1e39ef3a9bf908fcdb5e4c359ae607b33800f
Matheus Rodrigues Lima <matheus.rodrigues.lima@gmail.com>
2017-12-05 17:08:57 -0200

    Update Main.java

--- Hibernate/Hibernate_tutorial/src/Pessoa/Main.java
+++ Hibernate/Hibernate_tutorial/src/Pessoa/Main.java
@@ -1,6 +1,10 @@
 package Pessoa;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Scanner;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.hibernate.Session;
 
 import conexao.HibernateUtil;
@@ -12,7 +16,7 @@ public class Main {
 		Scanner entrada = new Scanner(System.in);
 		String linha;
 		
-		System.out.println("Digite seu nome:");
+		/*System.out.println("Digite seu nome:");
 		linha=entrada.nextLine();
 		person.setName(linha);
 		
@@ -25,13 +29,47 @@ public class Main {
 		person.setCpf(cpf);
 		
 		
-		
+		//Salva no banco
 		
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		session.beginTransaction();
 		session.save(person);
-		session.getTransaction().commit();
-		System.out.println("Teste");
+		session.getTransaction().commit();*/
+		
+		
+		//Pega do banco
+		/*Session sess = HibernateUtil.getSessionFactory().openSession();
+		List<Object[]> pessoas = sess.createSQLQuery("select * from pessoa").list();
+		
+		for(Object[] qry:pessoas)
+		{
+			System.out.println("Nome = " + qry[1]);
+		}
+		sess.getTransaction().commit()
+		sess.close();*/
+		
+		
+		//Excluindo item
+		/*Session sess = HibernateUtil.getSessionFactory().openSession();
+		sess.beginTransaction();
+		Query q = sess.createSQLQuery("delete from pessoa where id = 1");
+		q.executeUpdate();
+		sess.getTransaction().commit();
+		
+		sess.close();*/
+		
+		//Atualizando dado
+		Session sess = HibernateUtil.getSessionFactory().openSession();
+		sess.beginTransaction();
+		Query q = sess.createSQLQuery("update pessoa set nome = 'Fernanda' where nome = 'Matheus'");
+		q.executeUpdate();
+		sess.getTransaction().commit();
+		
+		sess.close();
+		
+		
+		
+		
 		
 	}
 }
